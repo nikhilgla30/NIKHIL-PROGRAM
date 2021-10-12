86e2aa3c2d65c40df8d193c5da005cbaeab4d36d
nikhilgla30 <91726392+nikhilgla30@users.noreply.github.com>
2021-10-12 20:20:16 +0530

    Add files via upload

---
+++ nikhilq14.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    char ch;
+    float f;
+    printf("Entre the integer:");
+    scanf("%d",&num);
+
+    printf("\nEntred integer is: %d ",num);
+    printf("\n\nEntre the float:");
+    scanf("%f",&f);
+    printf("\nEnter float is: %f",f);
+    printf("\n\nEntred the character: ");
+    scanf("%c",&ch);
+    printf("\nEntered integer is: %c",ch);
+
+    return 0;
+}
